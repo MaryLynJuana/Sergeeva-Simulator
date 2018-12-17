5ed77d861cf0859c6c4c663e3a37b1b70a0f4a09
MaryLynJuana <mary_juana@ukr.net>
2018-12-18 00:10:35 +0200

    Added interactive adding, deleting and correcting words in the dictionary. Made the tester ignore articles

--- sergeeva.js
+++ sergeeva.js
@@ -1,9 +1,8 @@
 'use strict'
 
+const fs = require('fs');
 const readline = require( 'readline' );
-const words = require('./words.js');
-
-const dictionary = words.dictionary;
+const dictionary = require('./words.json');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -16,20 +15,119 @@ const pickRandomKey = obj => {
 };
 
 const askTranslation = dict => {
-	console.log( 'To cancel the program, type "xx"' );
 	const rightWord = pickRandomKey( dict );
-	console.log( dict[ rightWord ] );
+	console.log(`\x1b[36m${dict[ rightWord ]}\x1b[0m`);
 	rl.question('Translate into English: ', answer => {
-		if ( answer.toUpperCase() === rightWord.toUpperCase() ) {
+		let aLow =  answer.trim().toLowerCase();
+		let rLow = rightWord.toLowerCase();
+		if (aLow === rLow || rLow === 'to ' + aLow || rLow === 'a ' + aLow || rLow === 'the ' + aLow) {
 			console.log('Right!')
-		} else if ( answer==='xx' ) {
+		} else if ( aLow === 'xx' ) {
 			rl.close();
+			console.clear();
 			return
+		} else if (aLow === 'menu') {
+			console.clear();
+			return menu(dict)
 		} else {
-			console.log(`Incorrect! \n Correct answer is: '${rightWord}'`)
+			console.log(`\x1b[31mIncorrect!\x1b[0m
+Correct answer is: \x1b[36m${rightWord}\x1b[0m`)
 		}
 	return  askTranslation( dict );
 	});
 };
 
-askTranslation( dictionary );
+const addNewWords = dict => {
+	rl.question('Type in English: ', eng => {
+		if (eng.trim().toLowerCase()==='xx') {
+			rl.close();
+			console.clear();
+			return
+		}
+		if (eng.trim().toLowerCase() === 'menu') menu(dict);
+		rl.question('Type in Ukrainian: ', ukr => {
+			if (ukr.trim().toLowerCase() === 'xx') {
+			rl.close();
+			console.clear();
+			return
+		}
+			if (eng, ukr) {
+				dict[eng] = ukr;
+				fs.writeFile('./words.json', JSON.stringify(dict), err => {
+					if (err) throw err
+					});
+				rl.close()
+			} else menu(dict)
+		});
+	});
+	
+};
+
+const deleteWord = dict => {
+	rl.question('Enter the word you want to delete: ', word => {
+		if (!dict[word]) menu(dict);
+		if ( word.trim().toLowerCase() === 'xx') {
+			rl.close();
+			console.clear();
+			return
+		}
+		rl.question(`Are you sure you want to delete the word ${word} from your dictionary?[y/n] `, confirm => {
+			if (confirm.trim().toLowerCase() === 'y') {
+				Reflect.deleteProperty(dict, word);
+				fs.writeFile('./words.json', JSON.stringify(dict), err => {
+					if (err) throw err;
+					console.log(`\x1b[36mThe word successfully deleted\x1b[0m`)
+				});
+			}
+			rl.close()
+		});
+	});
+}
+
+const correctWord = dict => {
+	rl.question(`\x1b[36mPlease enter the old English variant of the word you want to correct\x1b[0m
+Enter the uncorrected variant: `, old => {
+		Reflect.deleteProperty(dict, old);
+		console.log(`\x1b[36mNow enter the correct variant in English and in Ukrainian\x1b[0m`);
+		return addNewWords(dict);
+	})
+}
+
+const menu = dict => {
+	console.clear();
+	console.log( 
+	`\x1b[36mWelcome to Sergeeva Simulator!\x1b[0m
+To start a test, type 'start'
+To add new words to the dictionary, type 'add'
+To correct a mistake in a word from the dictionary, type 'cor'
+To delete a word from the dictionary, type 'del'
+To return to this menu, type 'menu'
+To cancel the program, type 'xx'` );
+	rl.question('Enter a command: ', command => {
+		console.clear();	
+		switch (command) {
+			case 'xx':
+				rl.close();
+				console.clear();
+				return;
+			case 'start':
+				askTranslation(dict);
+				break;
+			case 'add':
+				addNewWords(dict);
+				break;
+			case 'cor':
+				correctWord(dict);
+				break;
+			case 'del':
+				deleteWord(dict);
+				break;
+			case 'Shemsedinov':
+				console.log(`\x1b[38mVSEH NA PERERABOTKU\x1b[1m`);
+				rl.close()
+			
+		}
+	})
+};
+
+menu(dictionary);
