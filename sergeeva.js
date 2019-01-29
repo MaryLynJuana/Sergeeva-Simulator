bb89c4283b45332c01aeec405b17188428349313
MaryLynJuana <mary_juana@ukr.net>
2019-01-29 19:31:57 +0200

    Decompose functions

--- sergeeva.js
+++ sergeeva.js
@@ -9,125 +9,122 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-	
+
 const pickRandomKey = obj => {
 	const keys = Object.keys(obj);
-	const randomIndex = Math.floor(Math.random()*keys.length)
+	const randomIndex = Math.floor(Math.random()*keys.length);
 	return keys[ randomIndex ]
 };
 
-let correct = 0;
-let incorrect = 0;
+const toLower = str => str.trim().toLowerCase();
+
+const isRight = (word, rightWord) => {
+	const articles = [ '', 'a ', 'an ', 'the ', 'to ' ];
+	for (let article of articles) {
+		if (rightWord === article + word) return true
+	}
+	return false
+};
+
+const results = {
+	correct: 0,
+	incorrect: 0,
+	score: 0,
+};
 
 const askTranslation = dict => {
 	const rightWord = pickRandomKey(dict);
 	console.log(`\x1b[36m${dict[ rightWord ]}\x1b[0m`);
 	rl.question('Translate into English: ', answer => {
-		let aLow =  answer.trim().toLowerCase();
-		let rLow = rightWord.toLowerCase();
+		let aLow = toLower(answer);
+		let rLow = toLower(rightWord);
 		if (aLow in commands) return commands[aLow](dict);
-		let right = false;
-		const articles = [ '', 'a ', 'an ', 'the ', 'to ' ];
-		articles.forEach(article => {
-			if (rLow === article + aLow) right = true
-		});
-		if ( right ) {
+		if ( isRight(aLow, rLow) ) {
 			console.log('Right!');
-			correct++
+			results.correct++
 		} else {
-			console.log(`\x1b[31mIncorrect!\x1b[0m` +
-				`\nCorrect answer is: \x1b[36m${rightWord}\x1b[0m`);
-			incorrect++
-		};
+			const failMsg = `\x1b[31mIncorrect!\x1b[0m\nCorrect answer is: \x1b[36m${rightWord}\x1b[0m`;
+			console.log(failMsg);
+			results.incorrect++
+		}
 	return  askTranslation(dict);
 	});
 };
 
 const getResult = () => {
-	const count = correct + incorrect;
-	const score = count === 0 ? 0 : correct * 100 / count;
-	const res ={
-		correct,
-		incorrect,
-		score,
-	};
-	console.table(res);
-	if (score < 60) console.log(
-	`\x1b[35mCondratulations! You are on dopka\x1b[0m`
-	)
+	const count = results.correct + results.incorrect;
+	results.score = count ? results.correct * 100 / count : 0;
+	console.clear();
+	console.table(results);
+	const dopkaMsg = `\x1b[35mCondratulations! You are on dopka\x1b[0m`;
+	if (results.score < 60) console.log(dopkaMsg)
 };
 
-const addNewWords = dict => {
+//Functions for interactive adding, deleting and correcting words in the dictionary
+
+const rewriteDictionary = (dict, message) => {
+	fs.writeFile('./words.json', JSON.stringify(dict), () => {
+		rl.close();
+		console.log(message)
+	})
+};
+
+const addNewWord = dict => {
 	rl.question('Type in English: ', engWord => {
-		const eng = engWord.trim().toLowerCase();
+		const eng = toLower(engWord);
 		if (eng in commands) return commands[eng](dict);
 		rl.question('Type in Ukrainian: ', ukrWord => {
-		const ukr = ukrWord.trim().toLowerCase();
+			const ukr = toLower(ukrWord);
 			if (ukr in commands) return commands[ukr](dict);
-			if (eng && ukr) {
-				dict[eng] = ukr;
-				fs.writeFile('./words.json', JSON.stringify(dict), err => {
-					if (err) throw err;
-					rl.close();
-					console.log(
-						`\x1b[36mThe word ${eng} was successfully added\x1b[0m`
-					);
-				})
-			} else {
-				console.log(
-				`\x1b[31m$You have enter the word and its translation to add it\x1b[0m`
-				);
-				return addNewWords(dict)
+			const failMsg = `\x1b[31m$You have to enter the word and its translation to add it\x1b[0m`;
+			if (!eng || !ukr) {
+				console.log(failMsg);
+				return addNewWord(dict)
 			}
-		});
-	});
+			dict[eng] = ukr;
+			const successMsg = `\x1b[36mThe word ${eng} was successfully added\x1b[0m`;
+			rewriteDictionary(dict, successMsg)
+		})
+	})
 };
 
 const deleteWord = dict => {
 	rl.question('Enter the word you want to delete: ', engWord => {
-		const word = engWord.trim().toLowerCase();
+		const word = toLower(engWord);
 		if (word in commands) return commands[word](dict);
-		if (!dict[word]) menu(dict);
-		rl.question(
-			`Are you sure you want to delete the word \x1b[31m${word}\x1b[0m from your dictionary?[y/n] `,
-			confirm => {
-				const conf = confirm.trim().toLowerCase();
-				if (conf in commands) return commands[conf](dict);
-				if (conf === 'y') {
-					Reflect.deleteProperty(dict, word);
-					fs.writeFile('./words.json', JSON.stringify(dict), err => {
-						if (err) throw err;
-						rl.close();
-						console.log(`\x1b[36mThe word was successfully deleted\x1b[0m`)
-					});
-				} else {
-					console.log(
-						`\x1b[31m$There is no word \x1b[31m${word}\x1b[0m in the dictionary\x1b[0m`
-					);
-					return deleteWord(dict)
-				}
-			});
-	});
+		const failMsg = `\x1b[31mThere is no word \x1b[36m${word}\x1b[31m in the dictionary\x1b[0m`;
+		if (!dict[word]) {
+			console.log(failMsg);
+			return deleteWord(dict)
+		}
+		const confirmMsg = `Are you sure you want to delete the word \x1b[31m${word}\x1b[0m from your dictionary?[y/n] `;
+		rl.question(confirmMsg, confirm => {
+			const conf = toLower(confirm);
+			if (conf in commands) return commands[conf](dict);
+			if (conf !== 'y') return deleteWord(dict);
+			Reflect.deleteProperty(dict, word);
+			const successMsg = `\x1b[36mThe word was successfully deleted\x1b[0m`;
+			rewriteDictionary(dict, successMsg)
+		})
+	})
 };
 
 const correctWord = dict => {
-	console.log(
-		`\x1b[36mPlease enter the old English variant of the word you want to correct\x1b[0m`
-	);
+	const askOld = `\x1b[36mPlease enter the old English variant of the word you want to correct\x1b[0m`;
+	console.log(askOld);
 	rl.question(`Enter the uncorrected variant: `, oldWord => {
-				const old = oldWord.trim().toLowerCase();
+		const old = toLower(oldWord);
 		if (old in commands) return commands[old](dict);
 		Reflect.deleteProperty(dict, old);
-		console.log(
-			`\x1b[36mNow enter the correct variant in English and in Ukrainian\x1b[0m`
-		);
-		return addNewWords(dict);
+		const askCorrect = `\x1b[36mNow enter the correct variant in English and in Ukrainian\x1b[0m`;
+		console.log(askCorrect);
+		return addNewWord(dict);
 	})
 };
 
 const commands = {
 			
-	'add': dict => addNewWords(dict),
+	'add': dict => addNewWord(dict),
 	'cor': dict => correctWord(dict),
 	'del': dict => deleteWord(dict),
 	'menu': dict => menu(dict),
@@ -162,10 +159,8 @@ const menu = dict => {
 	console.log(menuMessage);
 	rl.question('Enter a command: ', command => {
 		console.clear();
-		if (command in commands) commands[command](dict);
-        else {
-			menu(dict)
-		}
+		if (command in commands) return commands[command](dict);
+        return menu(dict)
 	})
 };
 
